2f5d8cedd2a6754239307c9c7a7dcf74b789ce6f
Christosanesti8 <102608048+Christosanesti8@users.noreply.github.com>
2022-03-29 11:30:50 -0400

    Add files via upload

---
+++ Invoice.py
@@ -0,0 +1,29 @@
+# Brandon French
+# Invoice.py
+# March 29, 2022
+# calculates cost of books, tuition (3x credit hours), and lab fees.
+
+print ("*" *50)
+
+print ("\tColumbus State Community College \n\t500 East Spring Street \n\tColumbus, Ohio 43215")
+
+print ("-" *50)
+
+books = 52.99
+lab_fees = 25.00
+tuition = 157.93 * 3
+
+print ("\tProduct Name: \tProduct Price")
+print (f"\tBooks \t\t${books}")
+print (f"\tLab Fees \t${lab_fees}")
+print (f"\tTuition \t${tuition}")
+
+print ("-" *50)
+
+print (f"\t\t\tTotal \n\t\t\t${books + lab_fees + tuition}")
+
+print ("-" *50)
+
+print ("\n   Thank you for being a Columbus State Student!")
+
+print ("*" *50)
