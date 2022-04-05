323051af23bd9444d76c54861d8131223bb82ed4
Christosanesti8 <102608048+Christosanesti8@users.noreply.github.com>
2022-04-05 12:28:47 -0400

    Update multipliers.py

--- multipliers.py
+++ multipliers.py
@@ -1,34 +1,26 @@
 # Brandon French
 # french_lab4.py
 # March 29, 2022
-# inputs an operator and two numbers of the user's choice, and outputs them into an expression.
-while True:
+# inputs an operator and two numbers of the user's choice
+# and outputs them into an expression.
+
 operand = input ("Please select one option: add/subtract/multiply/divide: ")
 if operand == "divide" or operand == "subtract" or operand == "multiply" or operand == "add":
-print (f"You chose to {operand}!")
-break
+    print (f"You chose to {operand}!")
+    num_one = input ("Please select your first number: ")
+    float_one = float(num_one)
+    num_two = input ("Please select your second number: ")
+    float_two = float(num_two)
+    if operand == "divide":
+        print (f"{float_one} / {float_two} = {float_one/float_two}")
+    elif operand == "multiply":
+        print (f"{float_one} * {float_two} = {float_one*float_two}")
+    elif operand == "add":
+        print (f"{float_one} + {float_two} = {float_one+float_two}")
+    else:
+        print (f"{float_one} - {float_two} = {float_one-float_two}")
+    input(f'Thank you for using this calculator! Press "Enter" to exit.')
+
 else:
-print(f"Your input {operand} is incorrect. Please input one of the available options.")
-while True:
-try:
-num_one = input ("Please select your first number: ")
-float_one = float(num_one)
-break
-except:
-print ("Not an integer, please try again.")
-while True:
-try:
-num_two = input ("Please select your second number: ")
-float_two = float(num_two)
-break
-except:
-print ("Not an integer, please try again.")
-if operand == "divide":
-print (f"{float_one} / {float_two} = {float_one/float_two}")
-elif operand == "multiply":
-print (f"{float_one} * {float_two} = {float_one*float_two}")
-elif operand == "add":
-print (f"{float_one} + {float_two} = {float_one+float_two}")
-else:
-print (f"{float_one} - {float_two} = {float_one-float_two}")
-input(f'Thank you for using this calculator! Press "Enter" to exit.')
+    input(f'Your input {operand} is incorrect. Please press "Enter" to try again.')
+
